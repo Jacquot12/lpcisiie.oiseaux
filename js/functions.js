870860cd6147924872e9481b13d1ac681f2b3547
Outpox <guillaumeoutpox@gmail.com>
2015-03-16 15:51:35 +0100

    Commentaires + fonction qui renvoit l'url parente

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,20 @@
+/**
+ * Affiche tout le contenu du LocalStorage dans la console.
+ */
 function showLocalStorage() {
     console.group("LocalStorage");
     for (var j = 0; j < localStorage.length; j++) {
         console.log(localStorage.key(j) + ": " + localStorage.getItem(localStorage.key(j)));
     }
     console.groupEnd();
+}
+
+/**
+ * Renvoit l'url parente de là où est appelé la fonction.
+ * @return {string}
+ */
+function getParentUrl() {
+    var parentPath = window.location.pathname.split("/");
+    parentPath.pop();
+    return parentPath.join("/");
 }
