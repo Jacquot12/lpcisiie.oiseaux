02b12b42c754a23caaa756a94ae5b5612b9aa033
Outpox <guillaumeoutpox@gmail.com>
2015-03-18 11:16:25 +0100

    On stocke desormais les infos en json

--- js/newGame.js
+++ js/newGame.js
@@ -2,18 +2,9 @@ $.get('api/game', function (data) {
     //On vide le LocalStorage avant une nouvelle partie.
     localStorage.clear();
 
-    //Chaque Url de chaque question est stockée en LS avec une variable dynamiquement générée.
-    for (var i = 0; i < data.Nb_questions; i++) {
-        localStorage.setItem('Question' + i, data[i].Url);
-    }
+    //On stocke l'objet en JSON en LS pour le parse ensuite. Beaucoup plus simple et pratique qu'avant.
+    localStorage.setItem("data", JSON.stringify(data));
 
-    //On stocke les autres variables qui ne sont pas des objets en LS également.
-    //Permet de modifier plus facilement l'api car c'est dynamique.
-    for (var v in data) {
-        if (typeof data[v] !== "object") {
-            localStorage.setItem(v, data[v]);
-        }
-    }
     window.location.replace("game");
 });
 
