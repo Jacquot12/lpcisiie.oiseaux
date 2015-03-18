30951b8043dc107017189bf738e5ff5ff301dabc
Outpox <guillaumeoutpox@gmail.com>
2015-03-18 11:53:53 +0100

    Fonction nextLevel()

--- controller/apiJeu.php
+++ controller/apiJeu.php
@@ -7,7 +7,7 @@ use model\SousNiveau;
 /**
  * Nombre de questions par sous-niveau
  */
-const QUESTIONS_PAR_SS_NIVEAU = 10;
+const QUESTIONS_PAR_SS_NIVEAU = 5;
 
 /**
  * Niveau de base
@@ -26,7 +26,7 @@ class apiJeu {
      * Renvoit le json correspondant au premier sous-niveau et ses questions.
      */
     static function createNewGame(){
-        $questions = Question::select('Id_question')->distinct()->orderByRaw('RAND()')->limit(QUESTIONS_PAR_SS_NIVEAU)->get();
+        $questions = Question::select('Id_question')->distinct()->orderByRaw('RAND()')->limit(QUESTIONS_PAR_SS_NIVEAU)->where('Id_sous_niveau', '=', SOUS_NIVEAU)->get();
         foreach($questions as $q) {
             $q['Url'] = 'api/question/'.$q['Id_question'];
         }
@@ -37,6 +37,30 @@ class apiJeu {
         $questions['Sous_niveau'] = SOUS_NIVEAU;
         $questions['Sous_niveau_suivant'] = (int)$nb_points[0]['attributes']['Sous_niveau_suivant'];
         $questions['Utilisateur_points'] = 0;
+        echo json_encode($questions);
+    }
+
+    /**
+     * Renvoit le json correspondant au niveau suivant a celui fournit
+     *
+     * @param $niveau
+     * Niveau actuel
+     *
+     * @param $sous_niveau
+     * Sous niveau actuel
+     */
+    static function nextLevel($niveau, $sous_niveau) {
+        $nb_points = SousNiveau::select('Id_sous_niveau', 'Sous_niveau_suivant', 'Score_Validation', 'Sous_niveau_suivant')->where('Id_niveau', '=', $niveau)->where('Num_sous_niveau', '=', $sous_niveau)->get();
+        $questions = Question::select('Id_question')->distinct()->orderByRaw('RAND()')->limit(QUESTIONS_PAR_SS_NIVEAU)->where('Id_sous_niveau', '=', (int)$nb_points[0]['attributes']['Sous_niveau_suivant'])->get();
+        foreach($questions as $q) {
+            $q['Url'] = 'api/question/'.$q['Id_question'];
+        }
+        $questions['Nb_questions'] = QUESTIONS_PAR_SS_NIVEAU;
+        $questions['Nb_points_necessaires'] = (int)$nb_points[0]['attributes']['Score_Validation'];
+        $questions['Niveau'] = (int)$niveau;
+        $questions['Sous_niveau'] = (int)$sous_niveau;
+        $questions['Id_sous_niveau'] = (int)$nb_points[0]['attributes']['Id_sous_niveau'];
+        $questions['Sous_niveau_suivant'] = (int)$nb_points[0]['attributes']['Sous_niveau_suivant'];
         echo json_encode($questions);
     }
 }
