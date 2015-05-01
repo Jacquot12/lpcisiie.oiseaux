a97e63cd303f7fe724c595efdedfbff11c66b19e
Nicolas Obara <nicolas.obara@gmail.com>
2015-05-01 15:26:12 +0200

    update

--- sql/orni_question_sous_niveau.sql
+++
@@ -1,9 +0,0 @@
--- Le faire avec x de 2 à 10
-
-UPDATE orni_question
-SET Id_sous_niveau = x
-WHERE Id_question IN (SELECT *
-             FROM (SELECT Id_question
-                   FROM orni_question
-                   ORDER BY Id_question
-                   LIMIT (x-1)00, 100) AS t)
